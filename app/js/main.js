089360849218764a83fe226ae2cd90f1ca849644
bonchbrubonch <58067420+bonchbrubonch@users.noreply.github.com>
2024-09-12 16:13:45 +0300

    add desktop main page without footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,53 @@
 
 //toggle клас до блоку пошуку
 document.addEventListener('click', function (event) {
-    const searchIcon = document.querySelector('.header__search-icon');
-    const searchInner = document.querySelector('.header__search');
+  const searchIcon = document.querySelector('.header__search-icon');
+  const searchInner = document.querySelector('.header__search');
 
-    if (searchIcon.contains(event.target)) {
-        searchInner.classList.toggle('open');
-    }
-    else if (!searchInner.contains(event.target)) {
-        searchInner.classList.remove('open');
-    }
+  if (searchIcon.contains(event.target)) {
+    searchInner.classList.toggle('open');
+  }
+  else if (!searchInner.contains(event.target)) {
+    searchInner.classList.remove('open');
+  }
 });
 
 
 //sliders
 var swiper = new Swiper(".intro__slider", {
-    spaceBetween: 30,
-      effect: "fade",
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  spaceBetween: 30,
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".product-slider", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".review__slider", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+$(".rateYo_reviews").rateYo({
+  starWidth: "12px",
+  ratedFill: "#FFC700",
+  spacing: "2px",
+  rating: 5,
+  readOnly: true,
+});
