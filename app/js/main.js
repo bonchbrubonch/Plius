bc2e615fd787a0390b555b9e85d114606cbc1cc8
bonchbrubonch <58067420+bonchbrubonch@users.noreply.github.com>
2024-09-14 17:32:48 +0300

    add pages

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
+const navOffset = document.querySelector('.header').offsetTop + 200;
+
+window.addEventListener('scroll', function() {
+  const scrolled = window.scrollY;
+
+  if (scrolled > navOffset) {
+    document.querySelector('header').classList.add('sticky');
+  } else if (scrolled < navOffset) {
+    document.querySelector('header').classList.remove('sticky');
+  }
+});
+
 
 //toggle клас до блоку пошуку
 document.addEventListener('click', function (event) {
@@ -50,4 +62,22 @@ $(".rateYo_reviews").rateYo({
   spacing: "2px",
   rating: 5,
   readOnly: true,
+});
+
+$(".filter_by").select2({
+  minimumResultsForSearch: -1,
+  placeholder: "Current Number of Clients",
+});
+
+
+$(".category-listt__child-item").on("click", function () {
+  $(this).toggleClass("open");
+});
+
+$(".category-listt__chek").on("click", function () {
+  $(this).toggleClass("active");
+});
+
+$(".category-listt__title").on("click", function () {
+  $(this).toggleClass("active");
 });
